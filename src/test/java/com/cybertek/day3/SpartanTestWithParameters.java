8397352f86c8c4cc3fb0c335687a93134c2d9236
Rabia Tabassum <rabiatabbasam@yahoo.com>
2023-03-27 11:16:19 +0100

    Day 03: pathParam test 2

--- src/test/java/com/cybertek/day3/SpartanTestWithParameters.java
+++ src/test/java/com/cybertek/day3/SpartanTestWithParameters.java
@@ -49,7 +49,36 @@ public class SpartanTestWithParameters {
 
     }
 
+    /*
+            Given accept type is Json
+          And Id parameter value is 500
+          When user sends GET request to /api/spartans/{id}
+          then response status code should be 404
+          And response content-type: application/json
+          And "Not Found" should be in response payload
 
+     */
+
+    @DisplayName("GET request /api/spartan/{id} Negative Test ")
+   @Test
+    public void test2(){
+
+         Response response =   given().accept(ContentType.JSON)
+                                    .pathParam("id",500)
+                               .when()
+                                       .get("/api/spartans/{id}");
+
+         //verify status code
+         assertEquals(404,response.statusCode());
+
+         //verify content type
+         assertEquals("/application/json",response.contentType());
+
+         //verify Not Found in the json payload/body
+         assertTrue(response.body().asString().contains("Not Found"));
+
+
+}
 
 
 }
