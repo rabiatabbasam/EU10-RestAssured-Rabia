b55f03b0ff183d5eefb39b95a72adda81a80883f
Rabia Tabassum <rabiatabbasam@yahoo.com>
2023-03-27 13:40:50 +0100

    Day 03: SpartanTestsWithPath test1

---
+++ src/test/java/com/cybertek/day3/SpartanTestsWithPath.java
@@ -0,0 +1,71 @@
+package com.cybertek.day3;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.baseURI;
+
+import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SpartanTestsWithPath {
+    //BeforeAll is a annotation equals to @beforeClass in testNg, we use with static method name
+    @BeforeAll
+    public static void init(){
+        //save baseurl inside this variable so that we don't need to type each http method.
+        baseURI = "http://34.239.249.111:8000";
+
+    }
+
+    /*
+       Given accept type is json
+       And path param id is 10
+       When user sends a get request to "api/spartans/{id}"
+       Then status code is 200
+       And content -type is "application/json"
+       And response payload values match the following:
+          id is 10,
+          name is "Lorenza"
+          gender is "Female"
+          phone is 33121820936
+     */
+
+
+
+    @DisplayName("GET one spartan with Path Method")
+    @Test
+    public void test1(){
+
+        Response response = given().accept(ContentType.JSON)
+                .and().pathParam("id",10)
+                .when().get("api/spartans/{id}");
+
+        assertEquals(200,response.statusCode());
+        assertEquals("application/json",response.contentType());
+
+        //verify each json key has specific value
+        System.out.println(response.path("id").toString());
+        System.out.println(response.path("name").toString());
+        System.out.println(response.path("gender").toString());
+        System.out.println(response.path("phone").toString());
+
+        int id = response.path("id");
+        String name = response.path("name");
+        String gender = response.path("gender");
+        long phone = response.path("phone");
+
+        System.out.println("id = " + id);
+        System.out.println("name = " + name);
+        System.out.println("gender = " + gender);
+        System.out.println("phone = " + phone);
+        //assert the values
+        assertEquals(10,id);
+        assertEquals("Lorenza",name);
+        assertEquals("Female",gender);
+        assertEquals("33128209361",phone);
+
+    }
+}
