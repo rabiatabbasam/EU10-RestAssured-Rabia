3221f97a2b5b27ed991a3b68ba73778c1c42704a
Rabia Tabassum <rabiatabbasam@yahoo.com>
2023-04-02 18:11:43 +0100

    Day 05: SpartanAPIvsDB.java completed

--- src/test/java/com/cybertek/utilities/SpartanTestBase.java
+++ src/test/java/com/cybertek/utilities/SpartanTestBase.java
@@ -1,5 +1,6 @@
 package com.cybertek.utilities;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 
 import static io.restassured.RestAssured.baseURI;
@@ -11,5 +12,17 @@ public class SpartanTestBase {
         //save baseurl inside this variable so that we don't need to type each http method.
         baseURI = "http://34.239.249.111:8000";
 
+        String dbUrl = "jdbc:oracle:thin:@34.239.249.111:1521:XE";
+        String dbUsername = "SP";
+        String dbPassword = "SP";
+
+        DBUtils.createConnection(dbUrl,dbUsername,dbPassword);
+
+    }
+
+    @AfterAll
+    public static void teardown(){
+
+        DBUtils.destroy();
     }
 }
